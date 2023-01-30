6aa7cb701625eaef86e2e8e96ba5e38aaab42cc4
cedMac <mace.cedric@live.fr>
2023-01-30 23:47:35 +0100

    Ajout du cocktails

--- src/app/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-details/cocktail-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktail } from '../interfaces/cocktail.interface';
 
 @Component({
   selector: 'app-cocktail-details',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./cocktail-details.component.scss']
 })
 export class CocktailDetailsComponent {
-
+  cocktail: Cocktail = {
+      name: 'AVIATION',
+      img: 'https://1ou2cocktails.com/wp-content/uploads/2021/01/Cocktail_classique_Aviation_1ou2Cocktails_KP_06262019_3109-copie.jpg',
+      description:
+        'On prétend que ce cocktail fut baptisé d’après sa couleur bleue ciel proféré par la crème de violette. D’autres croient plutôt que son nom ferait référence à l’âge d’or de l’aviation aux États-Unis, l’époque à laquelle le cocktail est apparu. Commandez ce Sour classique à base de gin, de Luxardo Maraschino, de crème de violette et de jus de citron lors de votre prochaine soirée dans un bar à cocktail.',
+    
+  }
 }
