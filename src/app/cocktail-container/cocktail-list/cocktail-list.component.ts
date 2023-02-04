dca9dd7d0360a5d84ff2f53a9f7cef9bdb339668
cedMac <mace.cedric@live.fr>
2023-02-04 11:44:45 +0100

    Modification emplacement cocktail list

---
+++ src/app/cocktail-container/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Cocktail } from 'src/app/shared/interfaces/cocktail.interface';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.scss'],
+})
+export class CocktailListComponent implements OnInit {
+  @Input() public cocktails!: Cocktail[];
+  @Input() public selectedCocktail!: Cocktail;
+  @Output() private changeCocktail: EventEmitter<number> =
+    new EventEmitter<number>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public selectCocktail(index: number): void {
+    this.changeCocktail.emit(index);
+  }
+}
