4e1726aa3465154d472e1bcbfcab0f88f30e9d40
cedMac <mace.cedric@live.fr>
2023-02-04 11:47:30 +0100

    Creation directives selected

---
+++ src/app/shared/services/cocktail.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Cocktail } from '../interfaces/cocktail.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CocktailService {
+  public cocktails$: BehaviorSubject<Cocktail[]> = new BehaviorSubject([
+    {
+      name: 'Martini',
+      img: 'https://boiremixologie.com/files/medias/_thumb/capture-ecran-martini.jpg',
+      description:
+        '« Martini, shaken, not stirred », comme disait James Bond. Ce cocktail aurait été inventé après l’importation du vermouth italien Martini aux États-Unis. Un barman l’aurait alors mélangé à du gin, créant ainsi le cocktail le plus connu du monde.',
+    },
+    {
+      name: 'Margarita',
+      img: 'https://boiremixologie.com/files/medias/_thumb/margarita.jpg',
+      description:
+        'La margarita est un cocktail rafraichissant à base de tequila inventée en 1948 par Margaret Sames, d’où le nom (Margarita est la traduction en espagnol du prénom Margaret). Fait intéressant : la margarita est le cocktail le plus commandé en Amérique du Nord. ',
+    },
+    {
+      name: 'Old Fashioned',
+      img: 'https://boiremixologie.com/files/medias/_thumb/Old-fashioned.jpg',
+      description:
+        'Abordant d’abord le nom de Whiskey Cocktail, ce classique a été créé par un barman du Pendennis Club au Kentucky, qui ajouta du sucre au whiskey en l’honneur du Colonel James E. Pepper. Ce dernier apporta ensuite la recette au Waldorf-Astoria Hotel de New York. Le reste appartient à l’histoire. ',
+    },
+  ]);
+
+  public selectedCocktail$: BehaviorSubject<Cocktail> = new BehaviorSubject(
+    this.cocktails$.value[0]
+  );
+
+  public selectCocktail(index: number): void {
+      this.selectedCocktail$.next(this.cocktails$.value[index]);
+  }
+
+  constructor() {}
+}
