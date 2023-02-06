3890247db4a97e226e6e0b95100c74e4cf76c4d9
cedMac <mace.cedric@live.fr>
2023-02-06 16:01:11 +0100

    gestion d ajout d'ingredient

--- src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Cocktail } from 'src/app/shared/interfaces/cocktail.interface';
-
+import { PanierService } from 'src/app/shared/services/panier.service';
 
 @Component({
   selector: 'app-cocktail-details',
@@ -10,7 +10,11 @@ import { Cocktail } from 'src/app/shared/interfaces/cocktail.interface';
 export class CocktailDetailsComponent implements OnInit {
   @Input() public cocktail!: Cocktail;
 
-  constructor() {}
+  constructor(private panierService: PanierService) {}
 
   ngOnInit(): void {}
+
+  public addToPanier() {
+    this.panierService.addToPanier(this.cocktail.ingredients);
+  }
 }
