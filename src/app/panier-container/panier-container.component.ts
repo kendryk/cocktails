05f18f8a6889b861bebf2624a24a4a338feacf59
cedMac <mace.cedric@live.fr>
2023-02-06 16:02:05 +0100

    recuperation de la liste des ingredients et apport a la vue

--- src/app/panier-container/panier-container.component.ts
+++ src/app/panier-container/panier-container.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Ingredient } from '../shared/interfaces/ingredient.interface';
+import { PanierService } from '../shared/services/panier.service';
 
 @Component({
   selector: 'app-panier-container',
   templateUrl: './panier-container.component.html',
-  styleUrls: ['./panier-container.component.scss']
+  styleUrls: ['./panier-container.component.scss'],
 })
-export class PanierContainerComponent {
+export class PanierContainerComponent implements OnInit, OnDestroy {
+  public ingredients: Ingredient[] | null = null;
+  public _subscription: Subscription = new Subscription();
 
+  constructor(private panierService: PanierService) {}
+
+  ngOnInit() {
+    this._subscription = this.panierService.ingredients$.subscribe(
+      (ingredients: Ingredient[] | null) => {
+        (this.ingredients = ingredients)
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
 }
