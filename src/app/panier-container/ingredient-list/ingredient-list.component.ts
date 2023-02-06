4cfa9a8054d54480b807d9b7978ca9b6de126f39
cedMac <mace.cedric@live.fr>
2023-02-06 16:02:24 +0100

    visualisation du panier

--- src/app/panier-container/ingredient-list/ingredient-list.component.ts
+++ src/app/panier-container/ingredient-list/ingredient-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Ingredient } from 'src/app/shared/interfaces/ingredient.interface';
 
 @Component({
   selector: 'app-ingredient-list',
   templateUrl: './ingredient-list.component.html',
-  styleUrls: ['./ingredient-list.component.scss']
+  styleUrls: ['./ingredient-list.component.scss'],
 })
 export class IngredientListComponent {
-
+  @Input() public ingredients: Ingredient[] | null = null;
 }
