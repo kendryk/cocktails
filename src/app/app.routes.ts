202400afec0f65967f733f0b33f754355cb0a135
cedMac <mace.cedric@live.fr>
2023-02-07 08:46:08 +0100

    Création des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
-import { Routes } from "@angular/router";
-import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
-import { PanierContainerComponent } from "./panier-container/panier-container.component";
+import { Routes } from '@angular/router';
+import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
+import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
+import { PanierContainerComponent } from './panier-container/panier-container.component';
 
 export const APP_ROUTES: Routes = [
-    {path: '', redirectTo: 'cocktails', pathMatch: 'full'},
-    {path: 'cocktails', component:CocktailContainerComponent},
-    {path: 'panier', component:PanierContainerComponent}
-]
+  { path: '', redirectTo: 'cocktails', pathMatch: 'full' },
+  {
+    path: 'cocktails',
+    component: CocktailContainerComponent,
+    children: [
+      { path: ':index', component: CocktailDetailsComponent },
+      { path: '', redirectTo: '0', pathMatch: 'full' },
+    ],
+  },
+  { path: 'panier', component: PanierContainerComponent },
+];
